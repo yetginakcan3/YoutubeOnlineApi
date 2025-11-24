610256185716780f8c56f1171ca7546df408403b
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-11-24 12:26:39 +0300

    IRepository ve GenericRepository

---
+++ OnlineEdu.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineEdu.DataAccess.Abstract;
+using OnlineEdu.DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAccess.Repositories
+{
+    public class GenericRepository<T>(OnlineEduContext _context) : IRepository<T> where T : class
+    {
+        public DbSet<T> Table { get => _context.Set<T>(); }
+        
+        public List<T> GetList()
+        {
+            return Table.ToList();
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).FirstOrDefault();
+        }
+
+        public T GetById(int id)
+        {
+            return Table.Find(id);
+        }
+
+        public void Create(T entity)
+        {
+            Table.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            Table.Update(entity);
+            _context.SaveChanges();
+
+        }
+
+        public void Delete(int id)
+        {
+            var entity = Table.Find(id);
+            Table.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public int Count()
+        {
+            return Table.Count();
+        }
+
+        public int FilteredCount(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).Count();  
+        }
+
+        public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).ToList();
+        }
+
+        
+    }
+}
