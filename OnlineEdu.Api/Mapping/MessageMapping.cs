dd1e5c53353f80afd629910b9cd1a964ccf70fc2
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-11-26 10:49:26 +0300

    Messages, SocialMedias, Subscribers ve Testimonials Controller

---
+++ OnlineEdu.Api/Mapping/MessageMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.MessageDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.Api.Mapping
+{
+    public class MessageMapping : Profile
+    {
+        public MessageMapping()
+        {
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+        }
+
+
+    }
+}
