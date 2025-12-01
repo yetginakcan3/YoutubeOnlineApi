47630e2e70e61db57fb614d2188a6584a5771225
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-01 12:58:30 +0300

    Message Controller

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.MessageDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class MessageController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultMessageDto>>("Messages");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            await _client.DeleteAsync($"Messages/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+       
+
+        [HttpGet]
+        public async Task<IActionResult> MessageDetail(int id)
+        {
+           
+            var value = await _client.GetFromJsonAsync<ResultMessageDto>($"Messages/{id}");
+            return View(value);
+        }
+
+        
+    }
+}
