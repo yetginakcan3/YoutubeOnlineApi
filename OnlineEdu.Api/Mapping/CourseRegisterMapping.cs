347e3e290c1fafdf29af4d365693be14407ee743
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-12 10:19:50 +0300

    Course Register Endpointleri

---
+++ OnlineEdu.Api/Mapping/CourseRegisterMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.CourseRegisterDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.Api.Mapping
+{
+    public class CourseRegisterMapping:Profile
+    {
+        public CourseRegisterMapping()
+        {
+            CreateMap<ResultCourseRegisterDto, CourseRegister>().ReverseMap();
+
+            CreateMap<CreateCourseRegisterDto, CourseRegister>().ReverseMap();
+            CreateMap<UpdateCourseRegisterDto, CourseRegister>().ReverseMap();
+        }
+    }
+}
---
+++ OnlineEdu.WebUI/Areas/Student/CourseRegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.Areas.Student
+{
+    public class CourseRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
