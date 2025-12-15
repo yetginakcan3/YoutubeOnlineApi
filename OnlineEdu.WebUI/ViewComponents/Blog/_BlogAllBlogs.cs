1090372244bb0fb168319baa5de2092001806270
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-15 09:42:48 +0300

    Blog Sayfası Blog Listesi

---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogAllBlogs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.BlogDtos;
+using OnlineEdu.WebUI.Helpers;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogAllBlogs: ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultBlogDto>>("blogs");
+            return View(values);
+        }
+    }
+}
