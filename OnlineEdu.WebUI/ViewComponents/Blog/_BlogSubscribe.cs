d08f6b7eae88587817d4bbe7aa0714a9f0fbcef4
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-15 12:00:00 +0300

    Son Bloglar ve Abone Olma

---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogSubscribe.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogSubscribe:ViewComponent
+    {
+        
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+    }
+}
