5b2467576d22b228a5cc1634e7be7058db275ada
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-17 09:22:35 +0300

    Footer ve İletişim Sayfası

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactSendMessage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.MessageDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactSendMessage:ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+        
+    }
+}
