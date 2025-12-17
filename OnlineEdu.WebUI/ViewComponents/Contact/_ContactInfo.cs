65742eef9ee3205d380ba65b88efcd936d4d2fd4
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-12-17 09:44:34 +0300

    İletişim bilgileri ve harita bilgisi

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.ContactDtos;
+using OnlineEdu.WebUI.Helpers;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactInfo:ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+        }
+    }
+}
